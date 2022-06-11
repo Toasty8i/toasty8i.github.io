c7aaa494117a7606cae28b1709260677a6dfe01f
Toasty <76652414+Toasty8i@users.noreply.github.com>
2022-06-11 19:34:46 +0200

    testing

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -27,95 +27,200 @@ GitHub: https://github.com/Lumm1t/obnoxious.club
 
 */
 
-'use strict';
+"use strict";
 
-const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=65fb5e92df844197a0b309cfa6062ffb';
+const ipgeolocation =
+  "https://api.ipgeolocation.io/ipgeo?apiKey=65fb5e92df844197a0b309cfa6062ffb";
 
 const timeouts = [];
 
-const mobileAndTabletCheck = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+const mobileAndTabletCheck = () =>
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
 
 $(document).ready(() => {
   const links = [
     {
-      name: ' « steam »     ',
-      link: '76561198417487813',
+      name: " « steam »     ",
+      link: "76561198417487813",
     },
   ];
 
   for (let i in links) {
     let link = links[i];
 
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-    $('#marquee').append(`<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`);
-    $('#marquee').append(`<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`);
-    $('#marquee').append(`<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`);
-    $('#marquee').append(`<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`);
-      
-    link = $('#marquee').children('a').last();
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://steamcommunity.com/profiles/${link.link}" target="_BLANK">${link.name}</a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/qEKdBaz7VD" target="_BLANK"> « steam artworks »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://github.com/toasty8i" target="_BLANK"> « github »     </a>`
+    );
+    $("#marquee").append(
+      `<a href="https://discord.gg/NyVcunUFSt" target="_BLANK"> « discord »     </a>`
+    );
 
-    if (i != links.length - 1) $('#marquee').append(' <img class="emoticon" src="assets/others/mgh_17.png"> ');
+    link = $("#marquee").children("a").last();
+
+    if (i != links.length - 1)
+      $("#marquee").append(
+        ' <img class="emoticon" src="assets/others/mgh_17.png"> '
+      );
   }
 
   if (mobileAndTabletCheck()) {
-    $('#background').replaceWith('<div id="background" style="background-image: url(assets/images/mobile-background.jpg);"></div>');
+    $("#background").replaceWith(
+      '<div id="background" style="background-image: url(assets/images/mobile-background.jpg);"></div>'
+    );
 
     app.shouldIgnoreVideo = true;
   }
 
-  app.titleChanger(['toasty8i.github.io']);
-  app.iconChanger(['assets/icons/roses/rose1.jpg', 'assets/icons/roses/rose2.jpg', 'assets/icons/roses/rose3.jpg', 'assets/icons/roses/rose4.jpg']);
+  app.titleChanger(["toasty8i.github.io"]);
+  app.iconChanger([
+    "assets/icons/roses/rose1.jpg",
+    "assets/icons/roses/rose2.jpg",
+    "assets/icons/roses/rose3.jpg",
+    "assets/icons/roses/rose4.jpg",
+  ]);
 });
 
-if ($.cookie('videoTime')) {
-  app.videoElement.currentTime = $.cookie('videoTime');
-  app.audioElement.currentTime = $.cookie('videoTime');
-}
-
-document.addEventListener('contextmenu', (event) => {
+document.addEventListener("contextmenu", (event) => {
   event.preventDefault();
 });
 
@@ -133,15 +238,15 @@ document.body.onkeyup = (event) => {
   }
 };
 
-$('html').on('contextmenu', (event) => {
-  const img = document.createElement('img');
+$("html").on("contextmenu", (event) => {
+  const img = document.createElement("img");
 
-  const trollfaceLight = app.skippedIntro ? '' : 'trollface-light';
+  const trollfaceLight = app.skippedIntro ? "" : "trollface-light";
 
-  img.src = 'assets/others/trollface.jpg';
+  img.src = "assets/others/trollface.jpg";
   img.width = 64;
   img.height = 64;
-  img.alt = '`toasty8i.github.io';
+  img.alt = "`toasty8i.github.io";
   img.style = `position: absolute; left: ${event.pageX}px; top: ${event.pageY}px; z-index: 10`;
   img.className = `troll ${trollfaceLight}`;
 
@@ -149,16 +254,17 @@ $('html').on('contextmenu', (event) => {
 });
 
 setInterval(() => {
-  $('.troll').remove();
+  $(".troll").remove();
 }, 600);
 
-$('.skip').click(() => {
+$(".skip").click(() => {
   skipIntro();
 });
 
 $.fn.extend({
   animateCss: function (animationName) {
-    const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    const animationEnd =
+      "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
 
     this.addClass(`animated ${animationName}`).one(animationEnd, () => {
       $(this).removeClass(`animated ${animationName}`);
@@ -169,7 +275,7 @@ $.fn.extend({
 });
 
 const writeLine = (text, speed, timeout, callback) => {
-  timeout = typeof timeout === 'number' ? timeout : [0, (callback = timeout)];
+  timeout = typeof timeout === "number" ? timeout : [0, (callback = timeout)];
 
   const lineNumber = app.id !== 2 ? ++app.id : (app.id += 2);
 
@@ -183,36 +289,53 @@ const writeLine = (text, speed, timeout, callback) => {
 };
 
 $.getJSON(ipgeolocation, (data) => {
-  writeLine(['Authenticating...', "Granting access to <span style='font-size: 14px; color: #06d;'>[toasty8i.github.io]</span>..."], 30, () => {
-    if (app.skippedIntro) return;
-
-    clearCursor();
-
-    const usernames = ['user', 'dude'];
-
-    const ip = data.ip ? data.ip : usernames[Math.floor(Math.random() * usernames.length)];
-    const country = data.country_name ? data.country_name : 'your country';
-
-    writeLine([`Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>`, `Welcome back, <i style='color: #0f0'>${ip}</i>! By the way, nice to see someone from <i style='color: #0f0'>${country}</i> here!`], 30, 500, () => {
+  writeLine(
+    [
+      "Authenticating...",
+      "Granting access to <span style='font-size: 14px; color: #06d;'>[toasty8i.github.io]</span>...",
+    ],
+    30,
+    () => {
       if (app.skippedIntro) return;
 
       clearCursor();
 
-      writeLine([`<i style='color: #F62459'>Enjoy.</i>`], 120, 500, () => {
-        timeouts.push(
-          setTimeout(() => {
-            if (app.skippedIntro) return;
+      const usernames = ["user", "dude"];
 
-            clearCursor();
+      const ip = data.ip
+        ? data.ip
+        : usernames[Math.floor(Math.random() * usernames.length)];
+      const country = data.country_name ? data.country_name : "your country";
 
-            setTimeout(() => {
-              skipIntro();
-            }, 500);
-          }, 1000)
-        );
-      });
-    });
-  });
+      writeLine(
+        [
+          `Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>`,
+          `Welcome back, <i style='color: #0f0'>${ip}</i>! By the way, nice to see someone from <i style='color: #0f0'>${country}</i> here!`,
+        ],
+        30,
+        500,
+        () => {
+          if (app.skippedIntro) return;
+
+          clearCursor();
+
+          writeLine([`<i style='color: #F62459'>Enjoy.</i>`], 120, 500, () => {
+            timeouts.push(
+              setTimeout(() => {
+                if (app.skippedIntro) return;
+
+                clearCursor();
+
+                setTimeout(() => {
+                  skipIntro();
+                }, 500);
+              }, 1000)
+            );
+          });
+        }
+      );
+    }
+  );
 });
 
 const skipIntro = () => {
@@ -224,25 +347,27 @@ const skipIntro = () => {
     clearTimeout(timeout);
   });
 
-  $('.top-right').remove();
+  $(".top-right").remove();
 
-  $('#main').fadeOut(100, () => {
-    $('#main').remove();
+  $("#main").fadeOut(100, () => {
+    $("#main").remove();
 
-    $('#marquee').marquee({
+    $("#marquee").marquee({
       duration: 15000,
       gap: 420,
       delayBeforeStart: 1000,
-      direction: 'left',
+      direction: "left",
       duplicated: true,
     });
 
     setTimeout(() => {
-      $('.brand-header').animateCss(app.effects[Math.floor(Math.random() * app.effects.length)]);
+      $(".brand-header").animateCss(
+        app.effects[Math.floor(Math.random() * app.effects.length)]
+      );
     }, 200);
 
     setTimeout(() => {
-      const typed = new Typed('#brand', {
+      const typed = new Typed("#brand", {
         strings: app.brandDescription,
         typeSpeed: 40,
 
@@ -258,27 +383,22 @@ const skipIntro = () => {
         app.audioElement.play();
       }
 
-      app.videoElement.addEventListener(
-        'timeupdate',
-        () => {
-          
-        },
-        false
-      );
+      app.videoElement.addEventListener("timeupdate", () => {}, false);
 
-      $('.marquee-container').css('visibility', 'visible').hide().fadeIn(100);
+      $(".marquee-container").css("visibility", "visible").hide().fadeIn(100);
 
-      $('.marquee-container').animateCss('zoomIn');
+      $(".marquee-container").animateCss("zoomIn");
 
-      $('.container').fadeIn();
+      $(".container").fadeIn();
 
-      $('.background').fadeIn(200, () => {
-        if (!app.shouldIgnoreVideo) $('#audio').animate({ volume: app.musicVolume }, app.musicFadeIn);
+      $(".background").fadeIn(200, () => {
+        if (!app.shouldIgnoreVideo)
+          $("#audio").animate({ volume: app.musicVolume }, app.musicFadeIn);
       });
     }, 200);
   });
 };
 
 const clearCursor = () => {
-  return $('span').siblings('.typed-cursor').css('opacity', '0');
+  return $("span").siblings(".typed-cursor").css("opacity", "0");
 };
